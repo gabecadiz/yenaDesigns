9dc4a75d92203f023b0daca3a450135e4b385ed8
gabecadiz <gabecadiz@hotmail.com>
2020-07-24 11:42:34 -0400

    testing deployment

--- src/App.js
+++ src/App.js
@@ -4,29 +4,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='header'>
-        <div className='personal-info'>
-          <span className='name'>Yena Oh</span>
-          <span className='email'> yena2009@hotmail.com</span>
+    <div className="App">
+      <header className="header">
+        <div className="personal-info">
+          <span className="name">Yena Oh Test Deploy</span>
+          <span className="email"> yena2009@hotmail.com</span>
         </div>
 
-        <div className='hyper-links'>
-          <span className='work'>Work</span>
-          <span className='about'>About</span>
+        <div className="hyper-links">
+          <span className="work">Work</span>
+          <span className="about">About</span>
         </div>
       </header>
 
-      <div className='hero-image'>
-        <span className='hero-image-header'>Welcome to my Portfolio</span>
+      <div className="hero-image">
+        <span className="hero-image-header">Welcome to my Portfolio</span>
       </div>
-      <div className='hero-thrive'>
-        <span className='hero-thrive-header'>THRIVE</span>
+      <div className="hero-thrive">
+        <span className="hero-thrive-header">THRIVE</span>
       </div>
 
-      <footer className='footer'>
+      <footer className="footer">
         <div>FOLLOW ME ON LINKED</div>
-        <span className='watermark'>
+        <span className="watermark">
           Designed by Yena Oh in 2019. All rights reserved.
         </span>
       </footer>
