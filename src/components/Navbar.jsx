377b662e886c83d8c383108ebe76cdffd8f6258a
gabecadiz <gabecadiz@hotmail.com>
2020-07-26 12:24:49 -0400

    update folder structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import Icon from './images/Icon.svg';
+import Icon from '../images/Icon.svg';
 
 class Navbar extends Component {
   render() {
