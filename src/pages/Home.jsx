8854eeab8a99a79a4d141935ace4f1695e5f83b7
gabecadiz <gabecadiz@hotmail.com>
2020-08-16 21:11:10 -0400

    responsive picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+import ImageSmall from '../images/placeholder-small.JPG';
+import ImageMedium from '../images/placeholder-medium.JPG';
+import ImageLarge from '../images/placeholder-large.JPG';
+
+import Icon2 from '../images/Icon2.svg';
+import Icon from '../images/Icon.svg';
+
+const ResponsiveImage = () => (
+  <img
+    src={ImageMedium}
+    srcSet={`${ImageSmall} 600w, ${ImageMedium} 900w, ${ImageLarge} 1200w`}
+  />
+);
+
 class Home extends Component {
   render() {
     return (
       <div className="homepage">
         <h1 className="heading-primary">Home Page</h1>
+        {/* example of using bigger file for higher density screens */}
+        <picture className="homepage__wallpaper">
+          {/* picture element allows multiple sources for 1 image*/}
+          <ResponsiveImage />
+          <source
+            srcSet={`${ImageSmall} 1x, ${ImageSmall} 2x`}
+            media="max-width: 600px"
+          ></source>
+          <source
+            srcSet={`${ImageMedium} 1x, ${ImageMedium} 2x`}
+            media="max-width: 1200px"
+          ></source>
+        </picture>
         <span className="subtitle">Welcome to my website</span>
       </div>
     );
